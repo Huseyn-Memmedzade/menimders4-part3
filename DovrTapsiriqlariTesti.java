b94cd88ff6c6b7f6d2bb26d90183c7b30a5a9134
Backend Developer <hseyn.mmmdzad@bk.ru>
2025-07-17 19:57:11 +0400

    Add files via upload

---
+++ DovrTapsiriqlariTesti.java
@@ -0,0 +1,21 @@
+package yenipabka;
+
+import java.util.Scanner;
+
+public class DovrTapsiriqlariTesti {
+
+    public static void main(String[] args) {
+        // 3-dən 1-ə qədər geri say
+        // Hazirdir✅
+
+
+        int i = 4;
+        while (i > 1) {
+            --i;
+            System.out.println(i);
+        }
+    }
+}
+
+
+
